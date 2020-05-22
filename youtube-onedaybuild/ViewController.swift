363620b8b7bbaff1b294f1bbffff647ce5cb1310
Steve Kite <skite1@hotmail.com>
2020-05-22 06:59:02 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Steve on 5/22/20.
+//  Copyright © 2020 Steve Kite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
